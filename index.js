8287413660868ca214adb0f88019dfd204dda26f
Vicente Ternicien <38274115+vicho2t@users.noreply.github.com>
2019-08-26 18:05:15 -0400

    Finished v1

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
-
+//Funcion para recibir y enviar devuelta mensajes
 app.post('/message', function (req, res){
 	console.log(req.body);
 	var msgFrom = req.body.From;
 	var msgBody = req.body.Body;
 
+	console.log(msgFrom);
+	console.log(msgBody);
 
+	res.send(`
+		</Response>
+			<Message>
+				Hello ${msgFrom}. You said: ${msgBody}
+			</Message>
+		</Response>
+		`)
 })
+
+app.listen(3000);
