b638c3d753b96fad39e5d26af382686d925a36c8
Vicente Ternicien <38274115+vicho2t@users.noreply.github.com>
2019-08-27 17:40:50 -0400

    arreglo de puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const PORT = (process.env.PORT || 3000);
 
 var app = express();
 
